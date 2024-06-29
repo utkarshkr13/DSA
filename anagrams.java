c202e5b1fdd29d9c18c7eefa44d1ed9dea5a9128
Utkarsh Kumar Rajput <utkarshkr.13@gmail.com>
2024-06-30 04:02:40 +0530

    Gives a Boolean output to wheater the strings are anagram or not

---
+++ anagrams.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class anagrams {
+
+    public static Boolean anag(String first, String second){
+        char[] ch1 = first.toCharArray();
+        char[] ch2 = second.toCharArray();
+        Boolean result=false;
+
+        if(ch1.length!=ch2.length){
+            result= false;
+            return result;
+        }
+        else{
+            Arrays.sort(ch1);
+            Arrays.sort(ch2);
+
+            for(int i=0; i<ch1.length;i++){
+                if (ch1[i]==ch2[i]){
+                    result=true;
+                }
+                else{
+                    result=false;
+                    break;
+                }
+            }
+        }
+        return result;
+
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first String : ");
+        String a = sc.next();
+
+        System.out.println("Enter Second String : ");
+        String b = sc.next();
+
+        System.out.println(anag(a, b));
+
+
+    }
+    
+}
