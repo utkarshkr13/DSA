6a84e29a25fc08571b4136b6b5729d5a1c1c1b50
Utkarsh Kumar Rajput <utkarshkr.13@gmail.com>
2024-07-02 22:59:54 +0530

    Remove duplicate from sorted array

---
+++ removesup.java
@@ -0,0 +1,14 @@
+public class removesup {
+    
+    public int removeDuplicates(int[] arr) {
+        int i=0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[i]!=arr[j]){
+                i++;
+                arr[i]=arr[j];
+            }
+        }
+        return i+1;
+        
+    }
+}
