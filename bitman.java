a2d395224f78cd246c56c1855ab522bae1fbea7c
Utkarsh Kumar Rajput <utkarshkr.13@gmail.com>
2024-07-03 23:10:39 +0530

    Add 1 To the Bit

--- bitman.java
+++ bitman.java
@@ -1,3 +1,20 @@
+import java.util.*;
 public class bitman {
+    public static void oddoreven(int n){
+        int bitmask = 1;
+        if ((n & bitmask)==0){
+            //even number
+            System.out.println("Even Number");
+        }
+        else{
+            System.out.println("Odd Number");
+        }
+
+    }
+    public static void main(String args[]){
+        oddoreven(5);
+        oddoreven(2);
+        oddoreven(11);
+    }
     
 }
